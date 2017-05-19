c7cbc2f22b7deaed82177efdade847af9b656a7a
Aki Gao <xag9bb@virginia.edu>
2017-05-19 13:33:36 -0400

    update linting to handle html and newest babel features

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: 'airbnb',
-  plugins: [
-    'react',
-    'jsx-a11y',
-    'import',
-  ],
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  rules: {
+    'react/forbid-prop-types': [1, { forbid: ['any'] }],
+    'react/prop-types': 0,
+    'class-methods-use-this': [1, { exceptMethods: ['renderField'] }],
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'eslint-plugin-html'],
   env: {
     browser: true,
     node: true,
